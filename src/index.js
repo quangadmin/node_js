6cc203d806c87dea5a643aa760cd0e7250f8da5a
Quang <quangluong1000005@gmail.com>
2024-11-26 22:59:03 +0700

    nodejs-03

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 // Middleware
-app.use(morgan("combined"));
+app.use(
+  express.urlencoded({
+    extended: true, // Thêm các thông tin về request và response vào body của request
+  })
+);
+app.use(express.json());
+
+// Middleware
+//app.use(morgan("combined"));
 
 // Cấu hình template engine
 app.engine(
@@ -25,8 +33,20 @@ app.get("/trang-chu", (req, res) => {
 });
 
 //news
+
 app.get("/news", (req, res) => {
   res.render("news"); // Render file home.handlebars từ thư mục views
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  //lưu ý request.body dùng cho phương thức method là post: nhằm để bảo mật dữ liệu gửi server
+  console.log(req.body);
+  //còn request.body dùng cho phương thức method là get không bảo mật bằng post
+  console.log(req.query);
+  res.send("222");
 });
 
 // Start server
